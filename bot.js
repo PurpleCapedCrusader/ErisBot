28ae98a2d3d2d78697dccb3b880e0e02316aab1e
PurpleCapedCrusader <alexcotton@mac.com>
2019-07-16 22:45:18 -0700

    second commit

---
+++ bot.js
@@ -0,0 +1,24 @@
+﻿
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
