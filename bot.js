d595ac665ca573cce92b62d126cce90167e4033b
PurpleCapedCrusader <alexcotton@mac.com>
2020-03-02 00:27:55 -0800

    order and time

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 ﻿const Discord = require('discord.js');
 const config = require("./config.json");
 var lowerCase = require('lower-case');
+const getDateStamp = require('./timeStamp.js')
 const fs = require("fs");
 const bot = new Discord.Client();
 const PREFIX = config.prefix;
@@ -15,10 +16,20 @@ bot.godData = require("../ErisBot/godData.json");
 bot.on('message', message => {
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
+    if (message.content === '!react') {
+        message.react('521813888392626216');
+    }
+
     let args = (message.content.substring(PREFIX.length).split(" "));
-    console.log("TEST = " + godArray.indexOf(lowerCase(args[0])));
+    console.log (args);
+    let messageAuthorUsername = (message.author.username);
+    let messageAuthorDiscriminator = (message.author.discriminator);
+
+    console.log(messageAuthorUsername + messageAuthorDiscriminator + " looked up " + args + " # "+ godArray.indexOf(lowerCase(args[0])) + " at " + getDateStamp.time() + " on " + getDateStamp.date());
 
     if (godArray.indexOf(lowerCase(args[0])) >= 0 || godArray.indexOf(lowerCase(args[0])) <= 66) {
+        args[0] = lowerCase(args[0]);
+        console.log (args[0]);
 
         switch (args[0]) {
             case 'rules':
@@ -76,6 +87,147 @@ bot.on('message', message => {
 
             case 'log':
                 message.channel.send('Game logs are stored in this folder: **Settings > Storage > Files > Android > Data > com.Roxley.SantoriniGame > Files >** Then find the one with the right date and time.')
+                break;
+
+            case 'invite':
+                message.channel.send('Share this link to invite someone here: https://discordapp.com/invite/9PKUp7C')
+                break;
+
+            case 'test': //repeat after interval
+                var myInt = setInterval(function () {
+                    message.channel.send('Hello');
+                }, 3000);
+                break;
+
+            case 'time':
+                let ts = Date.now();
+                let date_ob = new Date(ts);
+                let seconds= date_ob.getSeconds();
+                let minutes= date_ob.getMinutes();
+                let hours = date_ob.getHours();
+                let date = date_ob.getDate();
+                let month = date_ob.getMonth() + 1;
+                let year = date_ob.getFullYear();
+                message.channel.send('The time is ' + hours + ':' + minutes + ':' + seconds + ' on ' + month + "-" + date + "-" + year)
+                break;
+
+            case 'order':    
+                message.channel.send(
+                    'The lower number goes first.\n\u200b' +
+                    ' \n\u200b' +
+                    'Achilles 10\n\u200b' +
+                    'Adonis 25\n\u200b' +
+                    'Aeolus	56\n\u200b' +
+                    'Aphrodite	57\n\u200b' +
+                    'Apollo	18\n\u200b' +
+                    'Ares 62\n\u200b' +
+                    'Artemis 53\n\u200b' +
+                    'Asteria 45\n\u200b' +
+                    'Atalanta 28\n\u200b' +
+                    'Athena 63\n\u200b' +
+                    'Atlas 40\n\u200b' +
+                    'Bellerophon 14\n\u200b' +
+                    'Bia 0\n\u200b' +
+                    'Castor & Pollux 13\n\u200b' +
+                    'Chaos 34\n\u200b' +
+                    'Charon 11\n\u200b' +
+                    'Charybdis 6\n\u200b' +
+                    'Chronus 2\n\u200b' +
+                    'Circe 17\n\u200b' +
+                    'Clio 39\n\u200b' +
+                    'Demeter 55\n\u200b' +
+                    'Dionysus 24\n\u200b' +
+                    'Eris 50\n\u200b' +
+                    'Eros 21\n\u200b' +
+                    'Europa & Talus 9\n\u200b' +
+                    'Gaea 32\n\u200b' +
+                    'Graeae 26\n\u200b' +
+                    'Hades 3\n\u200b' +
+                    'Harpies 20\n\u200b' +
+                    'Hecate 64\n\u200b' +
+                    'Hephaestus 27\n\u200b' +
+                    'Hera 5\n\u200b' +
+                    'Heracles 15\n\u200b' +
+                    'Hermes 12\n\u200b' +
+                    'Hestia 60\n\u200b' +
+                    'Hippolyta 47\n\u200b' +
+                    'Hydra 43\n\u200b' +
+                    'Hypnus 23\n\u200b' +
+                    'Iris 37\n\u200b' +
+                    'Jason 58\n\u200b' +
+                    'Limus 16\n\u200b' +
+                    'Maenads 41\n\u200b' +
+                    'Medea 19\n\u200b' +
+                    'Medusa 22\n\u200b' +
+                    'Minotaur 59\n\u200b' +
+                    'Morpheus 1\n\u200b' +
+                    'Nemesis 42\n\u200b' +
+                    'Nyx (Artemis) 51\n\u200b' +
+                    'Odysseus 29\n\u200b' +
+                    'Pan 38\n\u200b' +
+                    'Pegasus 54\n\u200b' +
+                    'Persephone 4\n\u200b' +
+                    'Polyphemus 61\n\u200b' +
+                    'Poseidon 35\n\u200b' +
+                    'Prometheus 46\n\u200b' +
+                    'Proteus 30\n\u200b' +
+                    'Scylla 36\n\u200b' +
+                    'Selene 8\n\u200b' +
+                    'Siren 44\n\u200b' +
+                    'Tartarus 0\n\u200b' +
+                    'Terpsichore 33\n\u200b' +
+                    'Theseus 7\n\u200b' +
+                    'Triton 52\n\u200b' +
+                    'Tyche 48\n\u200b' +
+                    'Urania 49\n\u200b' +
+                    'Zeus 31\n\u200b' +
+                    ' \n\u200b' +
+                    'From mlvanbie\'s Santorini Power Order Aid for 2-Player Games: https://boardgamegeek.com/filepage/176006/santorini-power-order-aid-2-player-games'
+                    )
+                break;
+                    
+            case 'avatar':
+                if (!message.mentions.users.size) {
+                    return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}> ${message.author.username}`);
+                }
+                const avatarList = message.mentions.users.map(user => {
+                    return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+                });
+                // send the entire array of strings as a message
+                // by default, discord.js will `.join()` the array with `\n`
+                message.channel.send(avatarList);
+                break;
+
+            case 'erisbot':
+            case 'help':
+                message.channel.send('Send a message where the first word starts with "!" and then, with no space, the name of a character in the game or one of her pre-programmed trigger words.\n\u200b' + 
+                ' \n\u200b' +
+                'She\'ll respond to:\n\u200b' +
+                '**!update-list** (list of powers updated for the app)\n\u200b' +
+                '**!rules** (game rules)\n\u200b' +
+                '**!force** (definition of force from rulebook)\n\u200b' +
+                '**!move** (definition of move from rulebook)\n\u200b' +
+                '**!build** (definition of build from rulebook)\n\u200b' +
+                '**!win** (definition of win from rulebook)\n\u200b' +
+                '**!dome** (definition of dome from rulebook)\n\u200b' +
+                '**!log** (location of log files on Android devices)\n\u200b' +
+                '**!board** (image of the board with space notation)\n\u200b' +
+                '**!invite** (discord invite link)\n\u200b' +
+                '**!order** (Power Order Aid for 2-Player Games)\n\u200b' +
+                '**!apollo** (information about Apollo... this works for all Gods and Heroes)\n\u200b' +
+                ' \n\u200b' +
+                'If you don\'t want to message her in a public channel, you can DM her and she\'ll respond to you privately.')
+                break;
+                
+            case 'board':
+                const file = new Discord.Attachment('../ErisBot/images/santoriniBoard.jpg');
+                const exampleEmbed = {
+                    title: 'Santorini Board Notation',
+                    image: {
+                        url: 'attachment://santoriniBoard.jpg',
+                    },
+                };
+                message.channel.send({ files: [file], embed: exampleEmbed });
                 break;
 
             case 'update-list':
